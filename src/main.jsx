e3c531bf19ced53e037cd0fb62d98cc102515f8a
AkshayG-mebs <agopinathan@millenniumci.com>
2023-05-10 22:17:41 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+
+import reactToWebComponent from "react-to-webcomponent";
+
+import App from "./App";
+
+const WebApp = reactToWebComponent(App, React, ReactDOMClient);
+
+customElements.define("web-app", WebApp);
