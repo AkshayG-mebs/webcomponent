8551bc7a88b44b1b933ca07c9dc4be80be28c610
AkshayG-mebs <agopinathan@millenniumci.com>
2023-05-11 01:36:42 +0530

    add rollup

--- predeploy.sh
+++
@@ -1,2 +0,0 @@
-# remove the version hash from our base javascript file for a stable URL
-find dist/assets -name "index-*.js" -exec mv '{}' dist/assets/main.js \;
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/webcomponent/",
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/main.js",
+        entryFileNames: "assets/main.js",
+      },
+    },
+  },
 });
