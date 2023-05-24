12b9ba3d0bb77837f6243608a98f869a7cdeffac
AkshayG-mebs <agopinathan@millenniumci.com>
2023-05-24 13:57:45 +0530

    update style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div style={{ position: "absolute", bottom: "10px", right: "20px" }}>
+    <div style={{ position: "fixed", bottom: "10px", right: "20px" }}>
       <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
         This is working fine
       </a>
