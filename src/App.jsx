ebe86503335e3685896436328375347cb73ec88a
AkshayG-mebs <agopinathan@millenniumci.com>
2023-05-24 13:41:08 +0530

    update style

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div style={{ position: "absolute", bottom: "10px", right: "20px" }}>
       <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
         This is working fine
       </a>
-    </>
+    </div>
   );
 }
 
